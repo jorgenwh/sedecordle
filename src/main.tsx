d3ca07c3a598dc904bfcb7b56556b1eb97c29e9a
jorgenwh <jorgenwhen@gmail.com>
2025-10-05 13:10:03 +0200

    initialize analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './index.css'
+import { analytics } from './config/firebase'
+
+// Initialize analytics (automatic page view tracking)
+console.log('Analytics initialized:', analytics)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
